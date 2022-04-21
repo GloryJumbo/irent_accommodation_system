d2849d7ae43d568e8f13ec5bcfee9be50039d9ed
BravoKamunyoghe <limitedkamunyoghebravo@gmail.com>
2022-04-22 00:40:35 +0300

    Update bookings.jsx
    Updating bookings
    
    Co-Authored-By: GloryJumbo <48089291+GloryJumbo@users.noreply.github.com>

--- src/components/views/bookings.jsx
+++ src/components/views/bookings.jsx
@@ -1,49 +1,81 @@
-import React from 'react'
+import React, { useEffect, useState } from "react"
+import axios from "axios"
 
 function Bookings() {
+    const [bookedRooms, getBookedRooms] = useState([])
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/irent/api/booked/rooms").then(rooms => {
+            getBookedRooms(rooms.data.bookings)
+        }).catch(err => console.error(err))
+    }, [bookedRooms])
+
+    const approveBooking = (e) => {
+        e.preventDefault()
+        
+        let pp = "Paid"
+        let nn = "Not Paid"
+        let ppn = e.target.id.split("-")[1]
+        
+        e.target.textContent === "Paid"? axios.patch("http://localhost:8080/irent/api/booked/rooms/"+nn+"&"+e.target.id.split("-")[0]+"&"+ppn).then(res=>{
+            console.log(res.data.message);
+        }).catch(err=>console.log(err)): axios.patch("http://localhost:8080/irent/api/booked/rooms/"+pp+"&"+e.target.id.split("-")[0]+"&"+ppn).then(res=>{
+            console.log(
+                
+                res.data.msg
+            );
+        }).catch(err=>console.log(err))
+        // console.log(e.target.textContent);
+    }
     return (
         <div className="container" style={{ marginTop: "20px" }}>
-            <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-        <div class="row gx-3 gy-2 align-items-center">
-            <div class="col-sm-3">
-                <input type="text" class="form-control" placeholder="Booked by" aria-label="House name" />
-            </div>
-            <div class="col-sm-3">
-                <input type="text" class="form-control" placeholder="Phone number" aria-label="Location name" />
-            </div>
-            <div class="col-sm-3">
-                
-                <select class="form-select" id="specificSizeSelect">
-                    <option value="1">Matiya</option>
-                    <option value="2">Flats</option>
-                    <option value="3">Tholo</option>
-                </select>
-            </div>
-            <div class="col-sm-3">
-                <button type="button" class="form-control btn btn-secondary">Save</button>
-            </div>
-        </div>
-<hr/>
-        <div>
-            <table class="table table-secondary">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
-                    </tr>
-                </thead>
-                
-            </table>
-        </div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+           
+            
+            <div>
+                <table className="table table-secondary">
+                    <thead>
+                        <tr>
+                            <th scope="col">Customer</th>
+                            <th scope="col">Room Number</th>
+                            <th scope="col">Phone Number</th>
+                            <th scope="col">Approve</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            bookedRooms.map((room, key) => {
+                                return <tr scope="col" key={key}>
+                                    <td id={room.id+"-"+room.phone_number} name={room}>
+                                        {room.customer_name}
+                                    </td>
+                                    <td id={room.id+"-"+room.phone_number} name={room}>
+                                        {room.room_numder}
+                                    </td>
+                                    <td id={room.id+"-"+room.phone_number} name={room}>
+                                        {room.phone_number}
+                                    </td>
+                                    <td id={room.id+"-"+room.phone_number} name={room}>
+                                        
+                                        <span id={room.id+"-"+room.phone_number} name={room} type="button" className="form-control btn btn-secondary" onClick={approveBooking}>{room.deposit_approval}</span>
+                                    </td>
+                                </tr>
+                            })
+                        }
 
-    </div>
+
+                    </tbody>
+                </table>
+
+            </div>
+
+        </div>
     )
 }
 
