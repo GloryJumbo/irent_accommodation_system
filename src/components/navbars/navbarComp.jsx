9c5dd50cc12d07bf8c78834fb7d3be97f80eded6
BravoKamunyoghe <limitedkamunyoghebravo@gmail.com>
2022-04-22 00:58:08 +0300

    Update navbarComp.jsx
    Adding nav bar
    
    Co-Authored-By: GloryJumbo <48089291+GloryJumbo@users.noreply.github.com>

--- src/components/navbars/navbarComp.jsx
+++ src/components/navbars/navbarComp.jsx
@@ -1,28 +1,42 @@
-import React from "react";
-import {Link} from "react-router-dom";
+import React,{useState, useEffect} from "react";
+import { Link } from "react-router-dom";
 
 function NavbarComp() {
+  const [user, getUser] = useState()
+
+  useEffect(()=>{
+
+      let us = JSON.parse(localStorage.getItem("username"))
+      getUser(us)
+
+  },[user])
   return (
     <div className="navbar-css position-fixed top-0 w-100">
-      <p>iRent Content Management System</p>
-      <div class="btn-group" role="group" aria-label="Basic example">
-
-       
-          <Link to="/houses"> <button type="button" class="btn btn-dark">Compounds</button></Link>
-          
-          <Link to="/rooms"><button type="button" class="btn btn-dark">
-            Rooms
-          </button></Link>
-          <Link to="/landlords"><button type="button" class="btn btn-dark">Landlords</button></Link>
-          <Link to="/bookings"><button type="button" class="btn btn-dark">Bookings</button></Link>
-          <Link to="/phone-numbers"><button type="button" class="btn btn-dark">Phonenumbers</button></Link>
-          <Link to="/statuses"><button type="button" class="btn btn-dark">Status</button></Link>
-        
-        
-        
-        
-        
+      <div className="st-css">
+        <span>{user}</span>
       </div>
+
+      <div style={{ textAlign: "center" }}>
+
+      <p>iRent Content Management System</p>
+      <div className="btn-group" role="group" aria-label="Basic example">
+
+
+        <Link to="/compounds"> <button type="button" className="btn btn-dark">Compounds</button></Link>
+
+        <Link to="/rooms"><button type="button" className="btn btn-dark">
+          Rooms
+        </button></Link>
+        <Link to="/landlords"><button type="button" className="btn btn-dark">Landlords</button></Link>
+        <Link to="/bookings"><button type="button" className="btn btn-dark">Bookings</button></Link>
+        <Link to="/phone-numbers"><button type="button" className="btn btn-dark">Phonenumbers</button></Link>
+        <Link to="/statuses"><button type="button" className="btn btn-dark">Status</button></Link>
+        <Link to="/compound-images"><button type="button" className="btn btn-dark">More Images</button></Link>
+
+
+      </div>
+      </div>
+
     </div>
   );
 }
