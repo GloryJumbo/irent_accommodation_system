b997578030c3d9272d5c1a49d17f05c39ddc6144
GloryJumbo <48089291+GloryJumbo@users.noreply.github.com>
2022-04-27 09:50:44 +0200

    Update AboutPage.jsx
    AboutPage Update

--- src/components/views/AboutPage.jsx
+++ src/components/views/AboutPage.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 function AboutPage() {
     return (
         <div className="container" style={{ marginTop: "20px" }}>
-          <p>We all know that accommodation is a basic need.<br>People move away from their respective homes.<br>There are many homes out there.<br>Let us find the one that is perfect for you.<br>Accommodation management information system is the web-based system that has come to make it easy for students find rooms and book and also to make facility owners to find and manage tenants as fast as possible using the web application.<br>The students will also easily explore the compound location and the available rooms for rent</p>
+          <p>We all know that accommodation is a basic need.
+            <br/>People move away from their respective homes.
+            <br/>There are many homes out there.
+            <br/>Let us find the one that is perfect for you.
+            <br/>Accommodation management information system is the web-based system that has come to make it easy for students find rooms and book and also to make facility owners to find and manage tenants as fast as possible using the web application.
+            <br/>The students will also easily explore the compound location and the available rooms for rent</p>
         </div>
     )
 }
